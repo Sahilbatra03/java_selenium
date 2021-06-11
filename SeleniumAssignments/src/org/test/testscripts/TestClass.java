6a4e16de1800cffc1275ed526d6a79c3342be6dd
sahil <sahil@LAPTOP-UF1VKL1T>
2021-06-11 12:40:19 +0530

    first commit

---
+++ SeleniumAssignments/src/org/test/testscripts/TestClass.java
@@ -0,0 +1,10 @@
+package org.test.testscripts;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is test method to push in git hub");
+	}
+
+}
